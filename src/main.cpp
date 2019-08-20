9c6337ecfdddf3aae8aa34f56dd889d0c376615a
zzthinker <zzrtbf@163.com>
2019-08-20 16:30:24 +0800

    VS GitHub extension test

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 int main(int argc, char** argv) {
 	
 	/*mpi handlers*/
-	int zz_try = 0;
+	int zz_try_again = 1;
 	int my_rank; //processor ID
 	int nb_proc; //number of processors
 	time_t start_time;
